d1f02a631d695a350dac05eef0d69480f0fecc7d
Theo Villeneuve <theovilleneuve.pro@gmail.com>
2023-03-24 11:18:47 +0100

    backend a déploy

--- models/connection.js
+++ models/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionString = 'mongodb+srv://alexandresossou:k8pliOfCEJXBZ3X7@cluster0.4hmkxzk.mongodb.net/Hackatweet1';
+const connectionString = process.env.CONNECTION_STRING;
 
 mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
   .then(() => console.log('Database connected'))
